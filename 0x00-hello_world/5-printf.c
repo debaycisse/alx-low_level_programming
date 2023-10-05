4ada71f34929b104fd8034d489f067a4c435182b
Azeez Adebayo <debaycisse@gmail.com>
2023-10-05 15:29:36 -0700

    Task 5. Hello, printf
    This .c file prints string of a set of characters on the stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Displays two phrases with commas
+ *
+ * Description: It displays two different phrases,
+ * each is seperated with comma
+ * Return: Always return 0 or success
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
