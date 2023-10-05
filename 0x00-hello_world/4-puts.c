6895ad6d45f584c9e500bac62812dea860db7263
Azeez Adebayo <debaycisse@gmail.com>
2023-10-05 15:09:38 -0700

    correction - task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - marks the entry point of the program
+ *
+ * Description: This prints a single double quote symbol, foolowed by and
+ * string of characters on the standard output
+ * Return: Always retun 0 (succuess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
