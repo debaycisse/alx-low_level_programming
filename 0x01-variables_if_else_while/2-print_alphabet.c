572f724170731efeed9090b175ecb239a75d9bb3
Azeez Adebayo <debaycisse@gmail.com>
2023-10-07 06:21:35 -0700

    Task 2.  I sometimes suffer from insomnia. And..
    I sometimes suffer from insomnia. And when I can't fall asleep,
    I play what I call the alphabet game
    This .c file prints a to z using two putchar()
    This task proves that letters are numbers by nature and
    represented by numbers

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main entry point into this program
+ *
+ * Description: it prints alphabet letters from a to z in lowercase
+ * Return: it returns 0, if the program runs successfuly
+ */
+ int main(void)
+{
+	char alphabet_letter = 'a';
+
+	while (alphabet_letter <= 'z')
+	{
+		putchar(alphabet_letter);
+		alphabet_letter++;
+	}
+	putchar('\n');
+	return (0);
+}
