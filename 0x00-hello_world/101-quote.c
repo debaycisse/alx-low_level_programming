f98df3375aaababb8df698118ddbe662ff7e3984
Azeez Adebayo <debaycisse@gmail.com>
2023-10-09 02:52:17 -0700

    advance task 8UNIX is basically a simple...
    UNIX is basically a simple operating system, but you have to be a genius
    to understand the simplicity.
    This .c file prints to standard error channel and returns 1 to
    activate this.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - entry point to the program
+ *
+ * Description: this program prints out to the standard error instead of
+ * standard output, using a function other than those mentioned in printf
+ * and puts manual section 345
+ * Return: returns 1 to generate an error whose so that the error message
+ * is accessible in the standard error stream
+ */
+int main(void)
+{
+	char *sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int sentence_length = strlen(sentence);
+
+	write(2, sentence, sentence_length);
+	return (1);
+}
