7dcd44bd3e3fdedabc728a5ac9a91c9e739c78ca
Azeez Adebayo <debaycisse@gmail.com>
2023-10-09 04:02:03 -0700

    correction - advance task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
  */
 int main(void)
 {
-	char *sentence = "and that piece of art is useful\" -\
+	char *sentence = "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n";
 	int sentence_length = strlen(sentence);
 
