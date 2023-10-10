acacdcf3c1ecdb40912b59cf311caab01673801b
Azeez Adebayo <debaycisse@gmail.com>
2023-10-10 13:23:22 -0700

    task 1. alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry to the main program
+ * @void: no parameter
+ *
+ * Description: prints a to z in lowercase using a
+ * given library, _putcha
+ * Return: 0 for success, 1 for error
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
