1a3c5c6fde4cf49e09850622f81b82831e1b5352
Azeez Adebayo <debaycisse@gmail.com>
2023-10-10 13:34:59 -0700

    correction - task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints a to z
+ * @void: no parameter
+ *
+ * Description: prints from a to z
+ * Return: no return
+ */
+void print_alphabet(void)
+{
+	int alph_letter = 97;
+	while (alph_letter <= 122)
+	{
+		_putchar((char) alph_letter);
+		if (alph_letter == 122)
+			_putchar('\n');
+		alph_letter++;
+
+	}
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,23 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/**
- * print_alphabet - prints a to z
- * @void: no parameter
- *
- * Description: prints from a to z
- * Return: no return
- */
-void print_alphabet(void)
-{
-	int alph_letter = 97;
-	while (alph_letter <= 122)
-	{
-		_putchar((char) alph_letter);
-		if (alph_letter == 122)
-			_putchar('\n');
-		alph_letter++;
-
-	}
-}
