355c6a5ecee5fbd01687c3a35bca9c616b277225
Azeez Adebayo <debaycisse@gmail.com>
2023-10-11 08:55:40 -0700

    task 10 a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integer together
+ * @n1: the first parameter to be added to n2.
+ * @n2: the second parameter to be added to n1.
+ *
+ * Description: adds two integer together and return their sum's value
+ * Return: returns the total of the two.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
