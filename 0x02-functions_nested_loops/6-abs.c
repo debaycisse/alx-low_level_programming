499a2403f5aacece2a59a4f83446631d29777836
Azeez Adebayo <debaycisse@gmail.com>
2023-10-11 01:17:11 -0700

    task 6 - absolute
    prints the absolute of a given integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 
+/**
+ * _abs - returns absolute form of a given integer
+ * @n: the parameter whose value is returned
+ *
+ * Description: returns the absolute form of any given integer
+ * Return: the absolute form of the given integer.
+ */
 int _abs(int n)
 {
 	return (abs(n));
