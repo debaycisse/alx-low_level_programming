6bf97acde325156fcba9a304a054bcd1ebf9e982
Azeez Adebayo <debaycisse@gmail.com>
2023-10-12 02:03:14 -0700

    task 0. Multiple mains
    This .c file is written to test the output of a function, named
    positive_or_negative() with a given test of zero or 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests a function as mentioned in the below description
+ *
+ * Description: tests the function, called positive_or_negative()
+ * with a value of 0. This function is supposed to print the given value with
+ * an additional information on whether the given value is positive,
+ * negative, or equal to zero.
+ */
+int main(void)
+{
+	int given_case;
+
+	given_case = 0;
+	positive_or_negative(given_case);
+
+	return (0);
+}
