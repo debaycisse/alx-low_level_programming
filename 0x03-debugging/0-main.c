d29dd7654596bf8b4a5cf6091009b6443c85ddda
Azeez Adebayo <debaycisse@gmail.com>
2023-10-12 02:33:12 -0700

    correction - task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests a function as mentioned in the below description
- *
+ * main - tests positive_or_negative() function
  * Description: tests the function, called positive_or_negative()
- * with a value of 0. This function is supposed to print the given value with
- * an additional information on whether the given value is positive,
- * negative, or equal to zero.
+ * with a value of 0.
  * Return: returns 0 if no error, otherwise 1
  */
 int main(void)
@@ -15,6 +12,5 @@ int main(void)
 
 	given_case = 0;
 	positive_or_negative(given_case);
-
 	return (0);
 }
