35f151ee557859a89d89da287491ecfc6078b47e
Azeez Adebayo <debaycisse@gmail.com>
2023-10-12 21:28:59 -0700

    task 3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Description: prints from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i = 48;
+	
+	while (i <= 57)
+	{
+		_putchar(i);
+		if (i == 57)
+			_putchar('\n');
+		i++;
+	}
+}
