d8a681a06006b6c3b74654b1208902a908f62d42
Azeez Adebayo <debaycisse@gmail.com>
2023-10-13 07:42:24 -0700

    task 9. Fizz-Buzz
    prints from 1 to 100
    For fizzbuzz question. Instead of printing each number:
      - prints fizz, if number is a divisible of 3,
      - prints buzz, if number is a divisible of 5
      - prints fizzbuzz, if number is a divisible of both 3 and 5
      - otherwise, prints a particular number

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("%s ", "FizzBuzz");
+		else if (i % 3 == 0)
+			printf("%s ", "Fizz");
+		else if (i % 5 == 0)
+			printf("%s ", "Buzz");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
