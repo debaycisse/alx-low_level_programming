a0b4af47222f89b8834194e35110297c74fd60a8
Azeez Adebayo <debaycisse@gmail.com>
2023-10-13 07:57:08 -0700

    correction - task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - entry point of the program
+ *
+ * Description: prints from 1 to 100, using FizzBuzz.
+ * Instead of printing a number, it prints:
+ * Fizz, if a number is divisible by 3,
+ * Buzz, if it's divisible by 5,
+ * FizzBuzz, if it's divisible by 3 and 5,
+ * otherwise, the number itself.
+ * Return: Always 0, if it runs successfully, 1, otherwise
+ */
 int main(void)
 {
 	int i;
