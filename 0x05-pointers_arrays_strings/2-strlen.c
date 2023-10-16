a0b607f73adefb670ec1724c289db62e23478236
Azeez Adebayo <debaycisse@gmail.com>
2023-10-16 10:52:48 -0700

    task 2. this report, by its very lengt...
    this report, by its very length, defends itself
    against the risk of being read
    
    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: the parameter for the string whose length is returned
+ *
+ * Description: computes the length of a string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
