6e05b5460cfe988173e0b63c23be036abc22fa80
Azeez Adebayo <debaycisse@gmail.com>
2023-10-16 08:37:15 -0700

    task 0. 98  Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - converts a pointer's value to 98
+ * @n: the reference to the pointer
+ *
+ * Description: takes an integer pointer and changes its value to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
