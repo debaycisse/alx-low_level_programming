17fcc4cdca9032ac50f47c92ada0abe2a2eb7525
Azeez Adebayo <debaycisse@gmail.com>
2023-10-17 11:03:21 -0700

    correction - task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string
+ * @str: string whose half is printed
+ * Description: prints half of a string
+ */
+void puts_half(char *str)
+{
+	int len = (int) strlen(str);
+	int _half = len / 2;
+	int i;
+	int j = _half;
+
+	if (len % 2 != 0)
+		_half = (len - 1) / 2;
+
+	for (i = 0; i <= _half; i++)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
