2bbe7021a82ecb20e7dc715dbaf71f7dfce50e0d
Azeez Adebayo <debaycisse@gmail.com>
2023-10-19 02:36:49 -0700

    correction - task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,18 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
-		if (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		else
-		{
-			dest[i] = '\0';
-			i++;
-		}
+		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
