fd0d379f4aa0cde90c03d534a136563cf5decf19
Azeez Adebayo <debaycisse@gmail.com>
2023-10-19 00:09:47 -0700

    cotrrection task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -22,11 +22,12 @@ char *_strncpy(char *dest, char *src, int n)
 		}
 		else
 		{
-			dest[i] = '\0';
+			/*dest[i] = '\0';*/
 			i++;
 		}
 	}
 	/*if (dest[i] != '\0')
 		dest[i] = '\0';*/
+	dest[i] = '\0';
 	return (dest);
 }
