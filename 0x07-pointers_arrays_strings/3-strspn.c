a116a6671fa952e3f8785231869e6b88e84f5eb5
Azeez Adebayo <debaycisse@gmail.com>
2023-10-24 21:31:12 +0100

    work in progress - task 3
    This is a work in progress on my task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,4 @@
-/*
-   Write a function that gets the length of a prefix substring.
-
-Prototype: unsigned int _strspn(char *s, char *accept);
-Returns the number of bytes in the initial segment of s which consist only of bytes from accept
-*/
-
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -16,48 +10,49 @@ Returns the number of bytes in the initial segment of s which consist only of by
  * all the characters in accept's pointer
  * Return: the length of the substring if found, otherwise 0 (zero)
 */
-int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	char *found_ch;
-	int found_cnt;
 	int i;
+	int from;
 	int j;
-	int substr_len;
-	int found_len = 0;
-	int k;
-
-	j = 0;
-	while (*(accept + j) != '\0')
-		j++;
+	char *accept_cpy = NULL;
+	unsigned int length = 0;
+	int a;
 
 	i = 0;
 	while (*(s + i) != '\0')
 	{
-		// Getting the length of the next substring in s
-		substr_len = 0;
-		while (*(s + substr_len) != 32)
-			substr_len++;
-		// check all characters of the sub-string for each character that makes up accept
-		k = 0;
-		while (k < j)
+		if (*(s + i) == 32)
 		{
-			found_cnt = 0;
-			while (l < substr_len++)
+			from++;
+			for (a = from; a < i; a++)
 			{
-				if (*(s + l)  == *(accept + k))
+				j = 0;
+				while (*(accept + j) != '\0')
 				{
-					*(found_ch + found_cnt) = *(accept + k);
-					found_cnt++;
-					l++;
+					if (*(accept + j) == *(s + (a - 1)))
+					{
+						*(accept_cpy + j) = *(accept + j);
+						j++;
+					}
+					else
+						j++;
 				}
-				else
-					l++;
-			}
 
-			if ((int) sizeof(found_ch) == j)
-				break;
-			else
-				k++;
+				if (sizeof(accept) == sizeof(accept_cpy))
+				{
+					length = (unsigned int) i - from;
+					break;
+				}
+				from++;
+			}
 		}
+
+		if (length > (unsigned int) 0)
+			break;
+
+		i++;
 	}
+
+	return (length);
 }
