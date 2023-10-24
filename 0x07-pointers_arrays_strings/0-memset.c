ef58e622974cf7492b66b83edeaceca324ba25e1
Azeez Adebayo <debaycisse@gmail.com>
2023-10-24 05:26:53 +0100

    update the task file
    update the task file to ensure that it is betty compatible syntactically.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,4 @@
-/*
-   Write a function that fills memory with a constant byte.
-
-Prototype: char *_memset(char *s, char b, unsigned int n);
-The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b
-Returns a pointer to the memory area s
-FYI: The standard library provides a similar function: memset. Run man memset to learn more.
-*/
-
+#include "main.h"
 /**
  * _memset - fills the first n bytes of the memory
  * @s: pointer to the memory location whose value spaces should be filled
