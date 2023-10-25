34676ab0b4088b932ce49eb5f1d5e0091e637251
Azeez Adebayo <debaycisse@gmail.com>
2023-10-25 19:12:55 +0100

    task 1. Why is it so important to dream? Becau...
    Why is it so important to dream? Because, in my dreams we are together
    
    This uses recursion in c language to print a set of characters
    in a reversed order

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,10 +9,11 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else if (*s != '\n')
+		return;
+	else
 	{
-		_print_rev_recursion(s++);
+		_print_rev_recursion(s+1);
 		_putchar(*s);
+		s++;
 	}
 }
