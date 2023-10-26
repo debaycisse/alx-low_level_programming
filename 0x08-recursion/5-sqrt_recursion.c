4bbcd505bee4d071a1b186fe1ea7b24beed2b569
Azeez Adebayo <debaycisse@gmail.com>
2023-10-26 03:31:10 -0700

    task 5.Your subconscious is looking for...
    Your subconscious is looking for the dreamer
    
    Finds and returns the natural square root of a number.
    Returns positive value for a number that has a whole number root,
    else return negative value for those that do not have a whole number
    as a root value.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
+int _sqrt_number_compute(int n, int begin_from);
 /**
  * _sqrt_recursion - computes the square root of a given number
- * @n: the number whose square root is computed
+ * @n: the number whose square root is to be computed
  *
  * Description: computes the square root of a number
  * Return: the value of the computed square root
@@ -11,12 +12,27 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n / 2 < 0)
-		return (-1);
 	if (n == 0)
-		return (0);
+		return (-1);
 	if (n == 1)
 		return (1);
-	return (_sqrt_recursion(n / 2));
-	n--;
+	return (_sqrt_number_compute(n, 2));
+}
+
+/**
+ * _sqrt_number_compute - computes square root finding computation
+ * @n: the number whose square root is to be computed
+ * @begin_from: the root to start from
+ *
+ * Return: a positive square root value, if n has a natural square root,
+ * otherwise negative one (-1)
+ */
+int _sqrt_number_compute(int n, int begin_from)
+{
+	if (begin_from * begin_from > n)
+		return (-1);
+	else if (begin_from * begin_from == n)
+		return (begin_from);
+	else
+		return (_sqrt_number_compute(n, ++begin_from));
 }
