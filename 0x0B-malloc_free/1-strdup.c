6c1a5e828093763d34fc642496fe1b1638db7449
Azeez Adebayo <debaycisse@gmail.com>
2023-10-30 11:12:50 -0700

    task 1. The woman who has no imagination...
    The woman who has no imagination has no wings
    
    duplicates a given string and returns a pointer to the new duplicates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a given string
+ * @str: the string to duplicate or copy
+ *
+ * Description: duplicates a string, contained in a given character pointer
+ * Return: pointer to the new memory location that houses the duplicates
+ */
+char *_strdup(char *str)
+{
+	char *str_dup;
+	int i;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+	size = 0;
+	while (str[size] != '\0')
+		size++;
+	str_dup = malloc(size * sizeof(*str_dup));
+	if (str_dup == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		str_dup[i] = str[i];
+		i++;
+	}
+	str_dup[i] = '\0';
+	return (str_dup);
+}
