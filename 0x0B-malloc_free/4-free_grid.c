91bbe8550eb6fe8d1a0b559270804954fcf4b78d
Azeez Adebayo <debaycisse@gmail.com>
2023-10-31 05:15:20 -0700

    add description for the function

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 
+/**
+ * free_grid - frees up the used dynamic memory space
+ * @grid: the pointer to the 2D array whose used memory space is to be freed
+ * @height: the number of the sub-array of the 2D array
+ *
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
