347f92cc99b08c00baa0810f82e361d0d3c8996e
Azeez Adebayo <debaycisse@gmail.com>
2023-11-01 06:50:31 -0700

    advance task 5. It isn't the mountains...
    It isn't the mountains ahead to climb that wear you out;
    it's the pebble in your shoe
    
    copies, concatenates passed parameters to mian function
    and returns the pointer to the copied, conactenated version.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**
+ * argstostr - concatenates the passed parameters
+ * @ac: the counter for the number of passed parameters
+ * @av: the multi-dimensional array to the passed parameters
+ *
+ * Description: copies and concatenates the passed parameters
+ * Return: the pointer to the concatenated string or null, if error.
+ */
 char *argstostr(int ac, char **av)
 {
 	char *new_av;
 	int i;
-	int j;
-	int k;
 	int sum;
+	int p_location;
+	int char_len;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -15,15 +23,27 @@ char *argstostr(int ac, char **av)
 	i = 1;
 	while (i < ac)
 	{
-		sum += strlen(av[i]) + 1;
+		char_len = (int)strlen(av[i]);
+		sum += char_len;
+		sum++;
 		i++;
 	}
 	sum++;
 	new_av = malloc(sum * sizeof(char));
+	if (new_av == NULL)
+		return (NULL);
+	p_location = 0;
 	i = 1;
 	while (i < ac)
 	{
-		strcat(new_av[i],av[i])
+		char_len = (int)strlen(av[i]);
+		strncpy(new_av + p_location, av[i], char_len);
+		p_location += char_len;
+		new_av[p_location] = '\n';
+		p_location++;
+		i++;
 	}
+	p_location++;
+	new_av[p_location] = '\0';
 	return (new_av);
 }
