2632ec1e066839c09d327042fe278c3adfeed522
Azeez Adebayo <debaycisse@gmail.com>
2023-11-06 09:07:28 -0800

    task 0. poppy
    an header file with double inclusion guarded and contains struct
    definition for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: data type to create dog instances
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
