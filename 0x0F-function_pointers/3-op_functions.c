ec1c46ea1fa73a4e05a2de892e73368a51704fdd
Azeez Adebayo <debaycisse@gmail.com>
2023-11-09 06:29:06 +0100

    work in progress - task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,4 +7,42 @@ op_mul: returns the product of a and b. Prototype: int op_mul(int a, int b);
 op_div: returns the result of the division of a by b. Prototype: int op_div(int a, int b);
 op_mod: returns the remainder of the division of a by b. Prototype: int op_mod(int a, int b);
 */
+/**
+ *
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
+/**
+ *
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
