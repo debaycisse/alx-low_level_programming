573161b43a7c6f8958a24daa8d49ae2cd6f1b348
Azeez Adebayo <debaycisse@gmail.com>
2023-11-09 20:03:47 +0100

    task 2. One woman's constant is another woman...
    One woman's constant is another woman's variable
    
    prints the given string, given to it.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,42 +3,26 @@
 #include <stddef.h>
 
 /**
-  Write a function that prints strings, followed by a new line.
-
-Prototype: void print_strings(const char *separator, const unsigned int n, ...);
-where separator is the string to be printed between the strings
-and n is the number of strings passed to the function
-You are allowed to use printf
-If separator is NULL, don’t print it
-If one of the string is NULL, print (nil) instead
-Print a new line at the end of your function
+ * print_strings - prints all the string, given to it
+ * @separator: string to use for separation between each string
+ * @n: the total number of the given strings
+ *
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list all_strings;
+	char *str;
 
 	va_start(all_strings, n);
 	for (i = 0; i < n; i++)
 	{
-		if ((i + 1 == n)
-			printf("%s", \
-					(va_arg(all_strings, char *) == NULL ? \
-					 "(nil)" : \
-					 va_arg(all_strings, char *)));
-
-		if (separator == NULL)
-			printf("%s", \
-					(va_arg(all_strings, char *) == NULL ? \
-					 "(nil)" : \
-					 va_arg(all_strings, char *)));
+		str = va_arg(all_strings, char *);
+		if ((i + 1 == n) || (separator == NULL))
+			printf("%s", (str == NULL ? "(nil)" : str));
 		else
-			printf("%s%s", \
-					(va_arg(all_strings, char *) == NULL? \
-					 "(nil)" : \
-					 va_arg(all_strings, char *)), \
-					 separator);
+			printf("%s%s", (str == NULL ? "(nil)" : str), separator);
 	}
 	va_end(all_strings);
 	printf("\n");
