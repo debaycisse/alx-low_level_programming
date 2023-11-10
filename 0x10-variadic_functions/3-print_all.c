aecfccf8af5572aea97d4d0b6b54687932ffdf18
Azeez Adebayo <debaycisse@gmail.com>
2023-11-10 01:51:19 -0800

    work in progress - task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,27 @@
 #include <stdio.h>
 #include <stddef.h>
 
+typedef struct format
+{
+	char format_op;
+	char (*f)(char);
+	void type;
+} fmt;
+
 void print_all(const char * const format, ...)
 {
+	fmt optrs[] = {
+		{'c', print_char, char},
+		{'i', print_int, int},
+		{'f', print_float, float},
+		{'s', print_string, char *}
+	}
+
+	while(i < 4)
+	{
+		if (optrs[i].format_op == format)
+			optrs[i].f(va_arg(all_vars, optrs[i].type);
+		i++;
+	}
 
 }
