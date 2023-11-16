9d774733496a17c9062bc0a848fb3e6b8997946b
Azeez Adebayo <debaycisse@gmail.com>
2023-11-16 09:47:45 -0800

    task 2. add node - done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,25 +3,20 @@
 #include <string.h>
 
 /**
-  Write a function that adds a new node at the beginning of a list_t list.
-
-Prototype: list_t *add_node(list_t **head, const char *str);
-Return: the address of the new element, or NULL if it failed
-str needs to be duplicated
-You are allowed to use strdup
+ * add_node - adds new node to the list
+ * @head: pointer to the memory location of the pointer that ppoints to head
+ * @str: the string or data to be stored
+ * Return: the pointer to the memory location of the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+
 	if (new == NULL)
 		return (NULL);
-	else
-	{
-		new->str = strdup(str);
-		new->len = strlen(str);
-		new->next = head;
-		head = new;
-		return(new);
-	}
-
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
