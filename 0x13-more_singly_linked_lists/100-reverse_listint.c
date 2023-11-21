909716a7fc4d2a675a902cc5956e32b063f68eb3
Azeez Adebayo <debaycisse@gmail.com>
2023-11-21 21:32:49 +0100

    implement task 11
    this function reverses the order of nodes in a list of nodes

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,13 +1,10 @@
 #include "lists.h"
 
 /**
- * Write a function that reverses a listint_t linked list.
-
-Prototype: listint_t *reverse_listint(listint_t **head);
-Returns: a pointer to the first node of the reversed list
-You are not allowed to use more than 1 loop.
-You are not allowed to use malloc, free or arrays
-You can only declare a maximum of two variables in your function
+ * reverse_listint - reverses the nodes in listint_t
+ * @head: pointer to the address of the head node
+ *
+ * Return: the head node for the reversed nodes
  */
 listint_t *reverse_listint(listint_t **head)
 {
@@ -23,7 +20,7 @@ listint_t *reverse_listint(listint_t **head)
 		*head = next_node->next;
 		if (*head == NULL)
 		{
-			*head = next;
+			*head = next_node;
 			next_node->next = previous_node;
 			previous_node = next_node;
 			next_node = *head;
