03158af6bc62487bb56410180b3ea781d2de039b
Azeez Adebayo <debaycisse@gmail.com>
2023-11-22 16:54:25 +0100

    work in progress - task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,19 +11,42 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_counts = 0;
-	const listint_t *each_node;
-	const listint_t *a_node = head->next;
-
+	const listint_t *fast_node = NULL;
+	const listint_t *slow_node = NULL;
+	
 	if (head == NULL)
 		exit(98);
-	each_node = head;
-	while (each_node->next != NULL)
+	slow_node = head;
+	fast_node = head->next;
+	while (1)
 	{
-		if ((node_counts > 0) && (each_node == a_node))
-			exit(98);
 		node_counts++;
-		printf("[%p] %d\n", (void *)each_node, each_node->n);
-		each_node = each_node->next;
+		printf("[%p] %d\n", (void *)slow_node, slow_node->n);
+		if (fast_node == NULL || fast_node->next == NULL)
+		{
+			if (slow_node != NULL)
+				slow_node = slow_node->next;
+		}
+		if (fast_node != NULL || fast_node->next != NULL)
+		{
+			if (fast_node == slow_node)
+			{
+				slow_node = head;
+				node_counts++;
+				printf("->[%p] %d\n", (void *)slow_node, slow_node->n);
+				return (node_counts);
+			}
+			if(fast_node->next == slow_node->next)
+			{
+			}
+			if (slow_node == slow_node->next)
+				return (node_counts);
+			if (fast_node != slow_node)
+			{
+				slow_node = slow_node->next;
+				fast_node = slow_node->next;
+			}
+		}
 	}
 	return (node_counts);
 }
