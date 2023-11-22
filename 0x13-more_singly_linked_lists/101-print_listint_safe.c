31c11c522d8703d18ed3682863d9f363aa1d05b7
Azeez Adebayo <debaycisse@gmail.com>
2023-11-22 06:33:37 +0100

    work in progress - task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,17 +12,18 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_counts = 0;
 	const listint_t *each_node;
+	const listint_t *a_node = head->next;
 
 	if (head == NULL)
 		exit(98);
 	each_node = head;
 	while (each_node->next != NULL)
 	{
+		if ((node_counts > 0) && (each_node == a_node))
+			exit(98);
 		node_counts++;
 		printf("[%p] %d\n", (void *)each_node, each_node->n);
 		each_node = each_node->next;
-		if (each_node->next == NULL)
-			exit(98);
 	}
 	return (node_counts);
 }
