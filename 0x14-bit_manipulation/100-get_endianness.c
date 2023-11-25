4014c350022eb52771502b218a3e0f0d33bc3368
Azeez Adebayo <debaycisse@gmail.com>
2023-11-25 05:56:19 +0100

    implement task 6
    function determines and return if system is
    using big or small endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - determines endianness
+ *
+ * Return: 1 if it is big endian, otherwise 0
+ */
+int get_endianness(void)
+{
+
+	int num = 1;
+	int num_s = sizeof(num);
+
+	if ((num_s & 1) == 1)
+		return (1);
+	else
+		return (0);
+}
