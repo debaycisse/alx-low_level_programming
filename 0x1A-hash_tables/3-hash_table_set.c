25a406c54ad0dcb373cd9f6ef567d61656715720
Azeez Adebayo <debaycisse@gmail.com>
2024-02-04 20:02:45 -0800

    work in progress...

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	// TODO: create a new hash_node_t 
+		// key MUST not be empty
+		// value MUST be duplicated; value can "" but not NULL
+	
+	// TODO: Obtain the index for the key
+
+	// TODO: add the created node to the table at the obtained index
+		// check for collision first AND handle it
+			// In case of collision, add the new node at the beginning of the list
+
+	// TODO: Returns: 1 if it succeeded, 0 otherwise
+
+
+}
