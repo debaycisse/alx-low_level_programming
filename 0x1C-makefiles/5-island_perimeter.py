049297794799b255c105fc6584db78f2a8937476
Azeez Adebayo <debaycisse@gmail.com>
2024-03-21 06:41:12 +0100

    complete implementation of all tasks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,15 +18,22 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if j == 0:
+            if (j == 0) and (i == 0):
                 if grid[i][j] == 1:
                     perimeter += 4
                     last_land_i = j
-            elif j > 0 and i == 0:
+            elif (j > 0) and (i == 0):
                 if grid[i][j] == 1:
                     perimeter += 4
                     if grid[i][j - 1] == 1:
                         perimeter -= 2
-            elif 
+            elif (j > 0) and (i > 0):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if grid[i][j - 1] == 1:
+                        perimeter -= 2
+                    if grid[i - 1][j] == 1:
+                        perimeter -= 2
+    return perimeter
                 
                     
