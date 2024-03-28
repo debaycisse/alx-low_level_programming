823d116a3537fabcd66b585234b2e2deb131d501
Azeez Adebayo <debaycisse@gmail.com>
2024-03-28 09:44:53 -0700

    correct logical keyword mis-spell

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,11 +17,11 @@ def island_perimeter(grid):
 
     perimeter = 0
     for row in range(len(grid)):
-        for col in range(len(grid[i])):
+        for col in range(len(grid[row])):
             if grid[row][col] == 1:
                 perimeter += 4
                 if (col > 0) and (grid[row][col - 1] == 1):
                     perimeter -= 2
-                if (row > 0) amd (grid[row - 1][col] == 1):
+                if (row > 0) and (grid[row - 1][col] == 1):
                     perimeter -= 2
     return perimeter
